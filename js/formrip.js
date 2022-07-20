9db09e0d542d1bd6d65d0f59f2878de47c90aa59
Facundo <facundocoto@hotmail.com>
2022-07-20 09:36:15 -0300

    Add gtm tags and confirm page

--- js/formrip.js
+++ js/formrip.js
@@ -4,12 +4,12 @@ function enviarFormulario() {
 
   var nombre_form = $("#input_nombre").val();
   var mail_form = $("#input_mail").val();
-  var mensaje_form = "Nuevo mensaje desde la web Bhuos"
+  var mensaje_form = "Nuevo mensaje desde la web Bhuos";
   var cel_form = $("#input_cel").val();
 
   var data = new FormData();
   // Bloqueando boton
-  $('.enviar').attr('value', 'Enviando...').attr('disabled', true);
+  $(".enviar").attr("value", "Enviando...").attr("disabled", true);
   // fin bloqueo
   data.append("key", valkey);
   data.append("message", mensaje_form);
@@ -30,13 +30,16 @@ function enviarFormulario() {
     method: "POST",
     contentType: false,
     processData: false,
-    data: data
-  }).done(function () {
-    $("input").attr("disabled", true).val("");
-    $("textarea").attr("disabled", true).val("");
-    $("#alert").addClass("mt-5").removeClass("d-none").addClass("show");
-    $('.enviar').attr('value', 'Enviado').attr('disabled', true).addClass("d-none");
-  }).fail(function () {
-    alert("Error al enviar");
-  });
+    data: data,
+  })
+    .done(function () {
+      location.href = "gracias.html";
+      // $("input").attr("disabled", true).val("");
+      // $("textarea").attr("disabled", true).val("");
+      // $("#alert").addClass("mt-5").removeClass("d-none").addClass("show");
+      // $('.enviar').attr('value', 'Enviado').attr('disabled', true).addClass("d-none");
+    })
+    .fail(function () {
+      alert("Error al enviar");
+    });
 }
